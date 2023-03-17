424d23d5b01b2f78b109beb9d09c1f5c460aa6f1
Katie Worton <katie.worton@linaro.org>
2023-03-17 20:00:35 +0000

    [WIP] Changes to scripts
    * Making updates to scripts to increase their usability outside of the
      skipfile testing. Changes are currently WIP

--- generate_skipfile_rerun_scripts.py
+++ generate_skipfile_rerun_scripts.py
@@ -14,6 +14,7 @@ import wget
 
 # example: ./generate_skipfile_rerun_scripts.py --test_type ltp --devices "qemu-armv7" "qemu-arm64" "qemu-i386" "qemu-x86_64"
 
+
 branch_tree_lookup_stable = {
     "linux-4.14.y": "linux-stable-rc",
     "linux-4.19.y": "linux-stable-rc",
@@ -29,6 +30,14 @@ branch_tree_lookup_other = {
     "linux-next": "master",
 }
 
+# some devices are named differently in the skipfiles
+skipfile_device_name_lookup = {
+    "qemu-armv7": "qemu_arm",
+    "qemu-arm64": "qemu_arm64",
+    "qemu-i386": "qemu_i386",
+    "qemu-x86_64": "qemu_x86_64",
+}
+
 all_qemu_devices = ["qemu-armv7", "qemu-arm64", "qemu-i386", "qemu-x86_64"]
 
 all_projects = [
@@ -38,9 +47,8 @@ all_branches = [b for b in branch_tree_lookup_stable.keys()] + [
     b for b in branch_tree_lookup_other.keys()
 ]
 
-compare_builds_url = "https://raw.githubusercontent.com/Linaro/squad-client-utils/master/squad-compare-builds"
 
-compare_builds_script_name = "squad_compare_builds.py"
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -74,8 +82,9 @@ def parse_args():
     parser.add_argument(
         "--other_accepted_build_names_regex",
         required=False,
-        default="gcc-\d\d-lkftconfig.*",
+        default=["gcc-\d\d-lkftconfig", "gcc-\d\d-lkftconfig.*"],
         help="If the preferred build name doesn't exist, allow build names that match this regex",
+        nargs="+",
     )
     parser.add_argument(
         "--test_type",
@@ -92,6 +101,12 @@ def parse_args():
         default=False,
         action="store_true",
         help="",
+    )
+    parser.add_argument(
+        "--allow_unrecognised_devices",
+        default=False,
+        action="store_true",
+        help=f"All devices which are not in the known devices list: {', '.join(all_qemu_devices)}",
     )
 
     return parser.parse_args()
@@ -124,208 +139,79 @@ def run():
     group = Squad().group(group_name)
     branches = args.branches
 
-    devices = args.devices
     environments_skipfile = ["all"]
 
-    # Grab download_tests from squad_compare_builds
-    if pathlib.Path(compare_builds_script_name).exists():
-        os.remove(compare_builds_script_name)
-    filename = wget.download(compare_builds_url, out=compare_builds_script_name)
-    print(filename)
-    from squad_compare_builds import download_tests
-    suite_names = ["ltp-syscalls"]
+
+    if not args.allow_unrecognised_devices and not all(device in all_qemu_devices for device in args.devices):
+        print(f"ERROR, unrecognised device(s): {set(args.devices).difference(set(all_qemu_devices))}")
+        print("Please check the device names supplied and add the --allow_unrecognised_devices if you wish to override this check")
+        sys.exit(1)
 
     for branch_name in branches:
         if branch_name in branch_tree_lookup_stable:
             project_name = (
                 f"{branch_tree_lookup_stable[branch_name]}-{branch_name}"
             )
-        elif branch_tree_lookup_other:
+        elif branch_name in branch_tree_lookup_other:
             project_name = (
                 f"{branch_name}-{branch_tree_lookup_other[branch_name]}"
             )
-
-        print(project_name)
-        project = group.project(project_name)
-        environments = [project.environment(environment) for environment in all_qemu_devices]
-        build_options = project.builds(count=5, ordering="-id")
-        # find the latest finished build - filtering for
-        # "finished=True" in the query doesn't seem to work
-        build = None
-        select_build_name = None
-        for build_option in build_options.values():
-            # only pick builds that are finished, unless explicitly allowed
-            if not build_option.finished and not args.allow_unfinished:
-                continue
-            # stop search if we have found build
-            if build:
-                break
-            test_result_filename = "test.txt"
-            suites = None
-            if suite_names:
-                suites = []
-                for s in suite_names:
-                    suites += project.suites(slug=s).values()
-            print(suites)
-            # download the test results from the suite we want and see if all devices were used
-            try:
-                download_tests(project=project, build=build_option, suites=suites, environments=environments, output_filename=test_result_filename)
-            except KeyError as e:
-                print("keyerror", e)
-                continue
-            test_results = pathlib.Path(test_result_filename).read_text(encoding="utf-8").split("\n")
-
-            device_build_names = {}
-
-            # find all the gcc versions for each arch and pick highest
-            for line in test_results:
-                if build:
-                    break
-                if len(line):
-                    # create a mapping to devices to build to look up if correct build exists
-                    device, build_name, suite_name, test_name_and_result = line.split("/")
-                    if device in device_build_names:
-                        device_build_names[device].add(build_name)
-                    else:
-                        print(device)
-                        device_build_names[device] = {build_name}
-
-            pprint.pprint(device_build_names)
-            if all(device in all_qemu_devices for device in device_build_names):
-                print("all devices accounted for")
-                for build_name_list in device_build_names.values():
-                    if not any(item in args.preferred_build_names for item in build_name_list):
-                        print(f"Couldn't find acceptable build name in {device_build_names.values()}")
-                        build = None
-                        select_build_name = None
-                        break
-                    else:
-                        print("keep checking")
-                        build = build_option
-                        select_build_name = None
-            else:
-                print("keep trying", device_build_names.keys())
-                continue
-        # try with diff, but regex
-        # TODO - put in function
-        print("try regex gcc-..-lkftconfig")
-
-        environments = [project.environment(environment) for environment in all_qemu_devices]
-        print(build_options)
-        build_options = project.builds(count=15, ordering="-id")
-        print(build_options)
-        if not build:
-            for build_option in build_options.values():
-                # only pick builds that are finished
-                if not build_option.finished and not args.allow_unfinished:
-                    continue
-                # stop search if we have found build
-                if build:
-                    break
-                test_result_filename = "test.txt"
-                suites = None
-                if suite_names:
-                    suites = []
-                    for s in suite_names:
-                        suites += project.suites(slug=s).values()
-                print(suites)
-                print(environments)
-                import traceback
-                try:
-                    download_tests(project=project, build=build_option, suites=suites, environments=environments, output_filename=test_result_filename)
-                except KeyError as e:
-                    print(traceback.format_exc())
-                    print("keyerror here", e)
-                    continue
-                test_results = pathlib.Path(test_result_filename).read_text(encoding="utf-8").split("\n")
-
-                device_build_names = {}
-
-                # find all the gcc versions for each arch and pick highest
-                for line in test_results:
-                    if build:
-                        break
-                    if len(line):
-                        device, build_name, suite_name, test_name_and_result = line.split("/")
-                        if device in device_build_names:
-                            device_build_names[device].add(build_name)
-                        else:
-                            print(device)
-                            device_build_names[device] = {build_name}
-
-                pprint.pprint(device_build_names)
-                if all(item in all_qemu_devices for item in device_build_names):
-                    print("all devices accounted for")
-                    for build_name_list in device_build_names.values():
-                        for build_name_option in build_name_list:
-                            if not re.match(args.other_accepted_build_names_regex, build_name_option):
-                                print(f"no match {build_name_option} regex")
-                                build = None
-                                continue
-                            else:
-                                print("found build name for this device regex")
-                                build = build_option
-                                break
-                        if not build:
-                            continue
-                else:
-                    print("keep trying", device_build_names.keys())
-                    continue
-
-        if build:
-            print("Found build", build.url)
         else:
-            print("No build found")
+            print(f"Error: branch name {branch_name} is not supported by this script")
+            sys.exit(1)
 
-            print(f"No build found for {branch_name}!")
-            print("logging issue in .issues_build.csv")
-            f = pathlib.Path(".issues_build.csv").open("a")
-            f.write(f"No build found for, {branch_name}!\n")
-            f.close()
+        print("Project name", project_name)
+        project = group.project(project_name)
+        environments = [project.environment(environment) for environment in args.devices]
 
-        print(build)
-        if build:
-            for device_name in devices:
-                for skipreason in skips["skiplist"]:
-                    print(skipreason["url"])
-                    # Create a skiplist for each reason in the skiplist
-                    single_reason_skiplist = {"skiplist": [skipreason]}
-                    for environment in environments_skipfile:
-                        skiptests = skipgen.get_skipfile_contents(
-                            board=device_name,
-                            branch=branch_name,
-                            environment=environment,
-                            skips=single_reason_skiplist,
-                        )
-                        # don't try rerunning if there are no tests to run for this branch
-                        if not skiptests:
-                            continue
+        # Check environments are all valid:
+        if None in environments:
+            print("Environment not found", [environment for environment in args.devices if not project.environment(environment)])
+            print("ERROR, check environments are valid")
+            sys.exit(1)
 
-                        import squad_generate_reproducer
+        for device_name in args.devices:
+            for skipreason in skips["skiplist"]:
+                print("Skipreason url", skipreason["url"])
+                # Create a skiplist for each reason in the skiplist
+                single_reason_skiplist = {"skiplist": [skipreason]}
+                for environment in environments_skipfile:
+                    skiptests = skipgen.get_skipfile_contents(
+                        board=device_name if device_name not in skipfile_device_name_lookup else skipfile_device_name_lookup[device_name],
+                        branch=branch_name,
+                        environment=environment,
+                        skips=single_reason_skiplist,
+                    )
+                    # don't try rerunning if there are no tests to run for this branch
+                    if not skiptests:
+                        continue
 
-                        # for test_name in skiptests:
-                        run_args = [
-                            "--group",
-                            group_name,
-                            "--project",
-                            project_name,
-                            "--build",
-                            build.version,
-                            "--device_name",
-                            device_name,
-                            "--build_name",
-                            select_build_name,
-                            "--test_type",
-                            args.test_type,
-                            "--retest_list_filename",
-                            args.retest_list_filename,
-                            "--debug",
-                            "--rerun_name",
-                            "-".join(skiptests),
-                            "--tests",
-                        ] + skiptests
+                    import squad_generate_reproducer
 
-                        squad_generate_reproducer.run(run_args)
+                    # for test_name in skiptests:
+                    run_args = [
+                        "--group",
+                        group_name,
+                        "--project",
+                        project_name,
+                        "--build",
+                        build.version,
+                        "--device_name",
+                        device_name,
+                        "--build_name",
+                        select_build_name,
+                        "--test_type",
+                        args.test_type,
+                        "--retest_list_filename",
+                        args.retest_list_filename,
+                        "--debug",
+                        "--rerun_name",
+                        "-".join(skiptests),
+                        "--tests",
+                    ] + skiptests
+
+                    squad_generate_reproducer.run(run_args)
+    print("Complete!")
 
 
 if __name__ == "__main__":
--- squad_generate_reproducer.py
+++ squad_generate_reproducer.py
@@ -11,7 +11,6 @@ from pathlib import Path
 from requests import HTTPError
 from squad_client.core.api import SquadApi
 from squad_client.core.models import Squad, Build, TestRun
-from squad_client.shortcuts import download_tests as download
 from squad_client.shortcuts import get_build
 from squad_client.utils import getid, first
 
@@ -22,12 +21,13 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def get_file(path):
+def get_file(path, filename=None):
     print(f"Getting file from {path}")
     if re.search(r"https?://", path):
         request = requests.get(path, allow_redirects=True)
         request.raise_for_status()
-        filename = path.split("/")[-1]
+        if not filename:
+            filename = path.split("/")[-1]
         with open(filename, "wb") as f:
             f.write(request.content)
         return filename
@@ -128,6 +128,85 @@ def parse_args(raw_args):
 
     return parser.parse_args(raw_args)
 
+def find_build(regex_exps, build_options, suite_names, environments, project, allow_unfinished, devices):
+    # download compare builds with a name that can be imported
+    compare_builds_url = "https://raw.githubusercontent.com/Linaro/squad-client-utils/master/squad-compare-builds"
+    compare_builds_script_name = "squad_compare_builds.py"
+    # Grab download_tests from squad_compare_builds
+    if pathlib.Path(compare_builds_script_name).exists():
+        os.remove(compare_builds_script_name)
+    filename = wget.download(compare_builds_url, out=compare_builds_script_name)
+    print("Filename", filename)
+    from squad_compare_builds import download_tests
+
+    build = None
+    select_build_name = None
+    for build_option in build_options.values():
+        # only pick builds that are finished
+        if not build_option.finished and not allow_unfinished:
+            continue
+        print("build option", build_option)
+        # stop search if we have found build
+        if build:
+            break
+        test_result_filename = "test.txt"
+        suites = None
+        if suite_names:
+            suites = []
+            for s in suite_names:
+                suites += project.suites(slug=s).values()
+        print("Suites", suites)
+        print("Environments", environments)
+        import traceback
+        try:
+            download_tests(project=project, build=build_option, suites=suites, environments=environments, output_filename=test_result_filename)
+        except KeyError as e:
+            print(traceback.format_exc())
+            print("keyerror here", e)
+            continue
+        test_results = pathlib.Path(test_result_filename).read_text(encoding="utf-8").split("\n")
+
+        device_build_names = {}
+
+        # find all the gcc versions for each arch and pick highest
+        for line in test_results:
+            if build:
+                break
+            if len(line):
+                device, build_name, suite_name, test_name_and_result = line.split("/")
+                if device in device_build_names:
+                    device_build_names[device].add(build_name)
+                else:
+                    print("device", device)
+                    device_build_names[device] = {build_name}
+
+        pprint.pprint(f"device build names {device_build_names}")
+        if all(item in device_build_names for item in devices):
+            print("all devices accounted for")
+            for regex_exp in regex_exps:
+                if build:
+                    break
+                for build_name_list in device_build_names.values():
+                    for build_name_option in build_name_list:
+                        re_match = re.match(f"^{regex_exp}$", build_name_option)
+                        if not re_match:
+                            print(f"no match {build_name_option} regex {regex_exp}")
+                            build = None
+                            select_build_name = None
+                            continue
+                        else:
+                            print("found build name for this device regex")
+                            build = build_option
+                            select_build_name = regex_exp
+                            break
+                    if not build:
+                        continue
+        else:
+            print("keep trying", device_build_names.keys())
+            continue
+
+    return build, select_build_name
+
 
 def run(raw_args=None):
     args = parse_args(raw_args)
@@ -162,6 +241,37 @@ def run(raw_args=None):
         print('There is no suite named "{suite_name}"')
         return -1
 
+    # get a build
+    suite_names = ["ltp-syscalls"]
+    build_options = base_project.builds(count=5, ordering="-id")
+    build = None
+    build, select_build_name = find_build(args.preferred_build_names, build_options, suite_names, environments, base_project, args.allow_unfinished, args.devices)
+    print("try regex gcc-..-lkftconfig")
+
+    print("build_options", build_options)
+    build_options = base_project.builds(count=15, ordering="-id")
+    print("build options", build_options)
+    if not build:
+        print("try again!!")
+        build, select_build_name = find_build(args.other_accepted_build_names_regex, build_options, suite_names, environments, base_project, args.allow_unfinished, args.devices)
+
+    if build:
+        print("Found build", build.url)
+    else:
+        print("No build found")
+
+        print(f"No build found for {branch_name}!")
+        print("logging issue in .issues_build.csv")
+        f = pathlib.Path(".issues_build.csv").open("a")
+        f.write(f"No build found for, {branch_name}!\n")
+        f.close()
+
+    print("Build", build)
+
+
+
+
+
     environment = base_project.environment(device_name)
     if environment is None:
         print(f'There is no environment for "{device_name}"')
@@ -170,67 +280,95 @@ def run(raw_args=None):
     test_options = build.tests(metadata=metadata.id, environment=environment.id)
     test = None
 
-    for test_option in test_options.values():
-        testrun = TestRun(getid(test_option.test_run))
-        if testrun.metadata.build_name == build_name:
-            test = test_option
-            print(f"Yes {testrun.metadata.build_name}")
-            break
-        else:
-            print(f"Not {testrun.metadata.build_name}")
-    # find a back up if needed
-    if not test:
-        alternatives = ["gcc-10-lkftconfig", "gcc-11-lkftconfig", "gcc-12-lkftconfig"]
+    # for test_option in test_options.values():
+    #     testrun = TestRun(getid(test_option.test_run))
+    #     if testrun.metadata.build_name == build_name:
+    #         test = test_option
+    #         print(f"Yes {testrun.metadata.build_name}")
+    #         break
+    #     else:
+    #         print(f"Not {testrun.metadata.build_name}")
+    # # find a back up if needed
+    # if not test:
+    #     alternatives = ["gcc-10-lkftconfig", "gcc-11-lkftconfig", "gcc-12-lkftconfig"]
+    #     for test_option in test_options.values():
+    #         testrun = TestRun(getid(test_option.test_run))
+    #         if testrun.metadata.build_name in alternatives:
+    #             test = test_option
+    #             print(f"Yes {testrun.metadata.build_name}")
+    #             break
+    #         else:
+    #             print(f"Not {testrun.metadata.build_name}")
+    # # find a back up if needed
+    # see if file exists that matches regex:
+    reproducer_regex = f"reproducer-{test_type}-{device_name}-{build.version}-({select_build_name}).sh"
+    found_build_name = None
+    for filename in os.listdir("."):
+        m = re.match(reproducer_regex, filename)
+        if m:
+            found_build_name = m.group(1)
+            print(found_build_name)
+    # if no reproducer found, grab test
+    if not found_build_name:
         for test_option in test_options.values():
             testrun = TestRun(getid(test_option.test_run))
-            if testrun.metadata.build_name in alternatives:
+            # prefer exact matches
+            if re.match(f"^{build_name}$", testrun.metadata.build_name):
                 test = test_option
-                print(f"Yes {testrun.metadata.build_name}")
+                print(f"Yes {testrun.metadata.build_name}, ^{build_name}$")
                 break
-            else:
-                print(f"Not {testrun.metadata.build_name}")
-    # find a back up if needed
-    if not test:
-        for test_option in test_options.values():
-            testrun = TestRun(getid(test_option.test_run))
-            if re.match("gcc-\d\d-lkftconfig.*", testrun.metadata.build_name):
-                test = test_option
-                print(f"Yes {testrun.metadata.build_name}")
-                break
-            else:
-                print(f"Not {testrun.metadata.build_name}")
+        if not test:
+            for test_option in test_options.values():
+                if re.match(f"{build_name}", testrun.metadata.build_name):
+                    test = test_option
+                    print(f"Yes {testrun.metadata.build_name}, {build_name}")
+                    break
+                else:
+                    print(f"Not {testrun.metadata.build_name}, {build_name}")
 
-    if test is None:
-        print(f'Build "{build.version}" has no test available on "{device_name}"')
-        return -1
-
-    # In theory there should only be one of those
-    testrun = TestRun(getid(test.test_run))
-    logger.debug(f"Testrun id {testrun.id}")
-    download_url = testrun.metadata.download_url
-    if download_url is None:
-        if testrun.metadata.config is None:
-            print("There is no way to determine download_url")
+        if test is None:
+            print(f'Build "{build.version}" has no test available on "{device_name}"')
             return -1
-        download_url = testrun.metadata.config.replace("config", "")
+
+        # In theory there should only be one of those
+        testrun = TestRun(getid(test.test_run))
+        logger.debug(f"Testrun id {testrun.id}")
+        download_url = testrun.metadata.download_url
+        if download_url is None:
+            if testrun.metadata.config is None:
+                print("There is no way to determine download_url")
+                return -1
+            download_url = testrun.metadata.config.replace("config", "")
 
     # Write the retest script name to a file if it isn't already there
     retest_filename = (
-        f"retest-{test_type}-{args.rerun_name}-{testrun.metadata.build_name}.sh"
+        f"retest-{test_type}-{args.rerun_name}.sh"
     )
     with open(args.retest_list_filename, "a+") as f:
         f.seek(0)
         retest_list = f.read()
         if retest_filename not in retest_list:
             f.write(retest_filename + "\n")
+
     for test_name in rerun_test_list:
         build_cmdline = ""
-        results_file = f"results-{test_type}-{test_name}-{device_name}-{build.version}-{testrun.metadata.build_name}.json"
-        log_file = f"log-{test_type}-{test_name}-{device_name}-{build.version}-{testrun.metadata.build_name}.txt"
-        print(testrun.job_url)
-        print(testrun.url)
+        if not found_build_name:
+            results_file = f"results-{test_type}-{test_name}-{device_name}-{build.version}-{testrun.metadata.build_name}.json"
+            reproducer_file = f"reproducer-{test_type}-{device_name}-{build.version}-{testrun.metadata.build_name}.sh"
+            log_file = f"log-{test_type}-{test_name}-{device_name}-{build.version}-{testrun.metadata.build_name}.txt"
+            print(testrun.job_url)
+            print(testrun.url)
+        else:
+            results_file = f"results-{test_type}-{test_name}-{device_name}-{build.version}-{found_build_name}.json"
+            reproducer_file = f"reproducer-{test_type}-{device_name}-{build.version}-{found_build_name}.sh"
+            log_file = f"log-{test_type}-{test_name}-{device_name}-{build.version}-{found_build_name}.txt"
+
         try:
-            tuxrun = get_file(f"{testrun.job_url}/reproducer")
+            if not Path(reproducer_file).exists():
+                tuxrun = get_file(f"{testrun.job_url}/reproducer", reproducer_file)
+            else:
+                print("reusing reproducer", reproducer_file)
+                tuxrun = reproducer_file
         except HTTPError:
             print(f"Reproducer not found at {testrun.job_url}!")
             print("logging issue in .issues.csv")
