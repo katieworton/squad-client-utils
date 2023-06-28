e8ad327bb79018329f449f48e2f69d28f386d776
Katie Worton <katie.worton@linaro.org>
2023-06-28 22:57:49 +0100

    [WIP] Updates to results reading for pipeline

--- run-and-push-skipfile-results.sh
+++ run-and-push-skipfile-results.sh
@@ -6,9 +6,14 @@ PROJECT_NAME=$2
 if [ "$#" -ne 2 ]; then
     echo "usage: ./run-and-push-skipfile-results <group_name> <project_name>"
 else
-    rm builds_for_skipfile_runs.txt
+
+    run_list=builds_for_skipfile_runs.txt
+    if test -f "$run_list"; then
+        rm "$run_list"
+    fi
     for PLAN in skipfile-reproducer*.yaml; do
         if test -f "$PLAN"; then
+            echo "$PLAN"
             tuxsuite plan $PLAN --json-out $PLAN.json --no-wait
             BUILD="$PLAN-$(date +'%s')"
             squad-client submit-tuxsuite --group=$GROUP_NAME --project=$PROJECT_NAME --build=$BUILD --backend tuxsuite.com --json $PLAN.json
