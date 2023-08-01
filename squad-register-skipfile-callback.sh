429348125af17a5c548d3788b468bfa0d4615047
Katie Worton <20370642+katieworton@users.noreply.github.com>
2023-08-01 13:50:44 +0100

    Update squad-register-skipfile-callback.sh
    Update build ID

--- squad-register-skipfile-callback.sh
+++ squad-register-skipfile-callback.sh
@@ -24,11 +24,7 @@ callback_url="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/jobs/${job_id}/play"
 #echo "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/jobs/${job_id}/play"
 echo "Callback URL: [${callback_url}]"
 
-ls
-echo *.yaml-[0-9]*
-
-BUILD_ID=*.yaml-[0-9]*
-sleep 2m
+echo "${BUILD_ID}"
 # Register callback with SQUAD
 if [ -z "${BUILD_ID}" ]; then
     source lib.sh
