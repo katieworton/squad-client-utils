c31cb87be5ea2542d4c5e745803cd0995071b83f
Katie Worton <katie.worton@linaro.org>
2023-08-09 13:58:48 +0100

    [WIP] Read results and update skipfile

--- run-and-push-skipfile-results.sh
+++ run-and-push-skipfile-results.sh
@@ -6,11 +6,19 @@ PROJECT_NAME=$2
 if [ "$#" -ne 2 ]; then
     echo "usage: ./run-and-push-skipfile-results <group_name> <project_name>"
 else
-    rm builds_for_skipfile_runs.txt
+
+    run_list=builds_for_skipfile_runs.txt
+    if test -f "$run_list"; then
+        rm "$run_list"
+    fi
     for PLAN in skipfile-reproducer*.yaml; do
-        tuxsuite plan $PLAN --json-out $PLAN.json --no-wait
-        BUILD="$PLAN-$(date +'%s')"
-        squad-client submit-tuxsuite --group=$GROUP_NAME --project=$PROJECT_NAME --build=$BUILD --backend tuxsuite.com --json $PLAN.json
-        echo $BUILD >>builds_for_skipfile_runs.txt
+        if test -f "$PLAN"; then
+            echo "$PLAN"
+            tuxsuite plan $PLAN --json-out $PLAN.json --no-wait
+            export BUILD_ID="$PLAN-$(date +'%s')"
+            squad-client submit-tuxsuite --group=$GROUP_NAME --project=$PROJECT_NAME --build=$BUILD_ID --backend tuxsuite.com --json $PLAN.json
+            echo $BUILD_ID >>builds_for_skipfile_runs.txt
+        fi
     done
 fi
+echo "build ${BUILD_ID}"
